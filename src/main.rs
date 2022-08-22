7d9fab8d5d25f92af53233e7732f3eef649c9455
etch1000 <110674620+etch1000@users.noreply.github.com>
2022-08-22 19:26:02 +0530

    Cars

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use rusty_engine::prelude::*;
+
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_label: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_label: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
+fn main() {
+    let mut game = Game::new();
+
+    // Setup game here
+
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+
+    player.translation = Vec2::new(-100.0, 0.0);
+    player.rotation = SOUTH_WEST;
+    player.scale = 1.5;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
+
+    game.add_logic(game_logic);
+
+    game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // Actual game logic
+    for event in engine.collision_events.drain(..) {
+        println!("{:#?}", event);
+    }
+
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
+}
